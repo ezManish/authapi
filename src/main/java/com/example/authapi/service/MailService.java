04a44e820c22ffce9f0072cd65ff2257456703c9
ezManish <manishkumarmgs019@gmail.com>
2025-12-10 15:06:55 +0530

    Add files via upload

--- src/main/java/com/example/authapi/service/MailService.java
+++ src/main/java/com/example/authapi/service/MailService.java
@@ -13,6 +13,7 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @org.springframework.scheduling.annotation.Async
     public void sendResetLink(String to, String resetLink) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
@@ -20,16 +21,15 @@ public class MailService {
         helper.setTo(to);
         helper.setSubject("Password Reset - Vittles");
 
-        String htmlContent =
-                "<div style='font-family:sans-serif'>" +
-                        "<h2>Password Reset Request</h2>" +
-                        "<p>Click the link below to reset your password:</p>" +
-                        "<a href='" + resetLink + "' target='_blank' " +
-                        "   style='background:#8B3358;color:#fff;" +
-                        "          padding:10px 16px;border-radius:6px;" +
-                        "          text-decoration:none'>Reset Password</a>" +
-                        "<p>This link expires in 15 minutes.</p>" +
-                        "</div>";
+        String htmlContent = "<div style='font-family:sans-serif'>" +
+                "<h2>Password Reset Request</h2>" +
+                "<p>Click the link below to reset your password:</p>" +
+                "<a href='" + resetLink + "' target='_blank' " +
+                "   style='background:#8B3358;color:#fff;" +
+                "          padding:10px 16px;border-radius:6px;" +
+                "          text-decoration:none'>Reset Password</a>" +
+                "<p>This link expires in 15 minutes.</p>" +
+                "</div>";
 
         helper.setText(htmlContent, true);
         mailSender.send(message);
